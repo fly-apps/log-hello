27bdced6c0190c5e91ee15cb0a75517f50e58041
Chris Nicoll <c@fly.io>
2023-02-09 20:18:14 -0500

    add waits; no need to exit

--- log-hello.go
+++ log-hello.go
@@ -1,13 +1,19 @@
 package main
 import (
         "os"
+        "time"
 )
-func main() {
-    println("======================")
-    println("")
+
+func printlogs() {
+    time.Sleep(100 * time.Millisecond)
+    println("=========================")
+    println("I'm a Fly Machine in app", os.Getenv("FLY_APP_NAME"))
     println("Hello from", os.Getenv("FLY_REGION"))
     println("Goodbye! Stopping Machine")
-    println("")
-    println("======================")
-	os.Exit(0)
+    println("=========================")
+}
+
+func main() {
+    defer time.Sleep(100 * time.Millisecond)
+    printlogs()
 }
