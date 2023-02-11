d4a7236dcb0e9e92259bb572d9c0cab83cc2a286
Chris Nicoll <c@fly.io>
2023-02-11 14:53:53 -0500

    dashboard logs strip extra whitespace

--- log-hello.go
+++ log-hello.go
@@ -2,15 +2,43 @@ package main
 import (
         "os"
         "time"
+        "strings"
 )
 
 func printlogs() {
+    time.Sleep(50 * time.Millisecond)
+    balloon := 
+    `
+    . . . . . . . . . . . .
+    . . . . . . . . . . . .
+    . . . ._. === ._. . . .
+    . . .'***'.|**\..'. . .
+    . ./***/...|***\..\\. .
+    . |*****...|****...|| .
+    . :****|...|***|...;; .
+    . .\****...|***...//. .
+    . . \***\..|**/..// . .
+    . . . \**\.|*/..' . . .
+    . . . . '*\|/.' . . . .
+    . . . . . /x\ . . . . .
+    . . . . .((_)). . . . .
+    . . . . . ''' . . . . .                 
+    `
+
+    for _, line := range strings.Split(strings.TrimRight(balloon, "\n"), "\n") {
+        time.Sleep(70 * time.Millisecond)
+        println(line)
+     }
     time.Sleep(100 * time.Millisecond)
-    println("=========================")
-    println("I'm a Fly Machine in app", os.Getenv("FLY_APP_NAME"))
-    println("Hello from", os.Getenv("FLY_REGION"))
-    println("Goodbye! Stopping Machine")
-    println("=========================")
+    println("   ==============================")
+    time.Sleep(100 * time.Millisecond)
+    println("   Hello from", os.Getenv("FLY_REGION"))
+    time.Sleep(100 * time.Millisecond)
+    println("   I'm a Fly Machine in app", os.Getenv("FLY_APP_NAME"))
+    time.Sleep(100 * time.Millisecond)
+    println("   Goodbye! Stopping Machine")
+    time.Sleep(100 * time.Millisecond)
+    println("   ==============================")
 }
 
 func main() {
