440f80cd838c5eab44be2c4e2c5be00c379d7d0e
Chris Nicoll <c@fly.io>
2023-02-13 13:21:34 -0500

    add countdown, keep animation

--- log-hello.go
+++ log-hello.go
@@ -8,40 +8,52 @@ import (
 func printlogs() {
     time.Sleep(50 * time.Millisecond)
     balloon := 
-    `
-    . . . . . . . . . . . .
-    . . . . . . . . . . . .
-    . . . ._. === ._. . . .
-    . . .'***'.|**\..'. . .
-    . ./***/...|***\..\\. .
-    . |*****...|****...|| .
-    . :****|...|***|...;; .
-    . .\****...|***...//. .
-    . . \***\..|**/..// . .
-    . . . \**\.|*/..' . . .
-    . . . . '*\|/.' . . . .
-    . . . . . /x\ . . . . .
-    . . . . .((_)). . . . .
-    . . . . . ''' . . . . .                 
-    `
+    ` . . . . . . . . . . . . . . . .
+ . /\/\. . . . . . . . . . . . .
+ . . . . . . . . . . .^^ . . . .
+ . . . . . _. === ._ . . . . . .
+ . . . . ,'*;'.|**\.'\ . . . . .
+ . . . ./**/...|***\.\\. . . . .
+ . . . .|**|...|***|.||. . . . .
+ . . . . \**\..|**/.// . . . . .
+ ^^. . . .'**..|*/./'. . . . . .
+ . . . . . .'*\|/.'. . . . . . .
+ . . . . . . . X . . . . . . . .
+ . . . . . . .(O). . . . . . . .
+ . . . . . . . . . . . /¯\v/¯\ .
+ . . . . . . . . . . . . . . . .`
 
+    println("3...")
+    time.Sleep(time.Second)
+    println("2...")
+    time.Sleep(time.Second)
+    println("1...")
+    time.Sleep(time.Second)
     for _, line := range strings.Split(strings.TrimRight(balloon, "\n"), "\n") {
-        time.Sleep(70 * time.Millisecond)
         println(line)
+        time.Sleep(50 * time.Millisecond)
      }
-    time.Sleep(100 * time.Millisecond)
-    println("   ==============================")
-    time.Sleep(100 * time.Millisecond)
-    println("   Hello from", os.Getenv("FLY_REGION"))
-    time.Sleep(100 * time.Millisecond)
-    println("   I'm a Fly Machine in app", os.Getenv("FLY_APP_NAME"))
-    time.Sleep(100 * time.Millisecond)
-    println("   Goodbye! Stopping Machine")
-    time.Sleep(100 * time.Millisecond)
-    println("   ==============================")
+    println(" ======= Hello from", os.Getenv("FLY_REGION"), "========")
+    time.Sleep(50 * time.Millisecond) 
+    println(" I'm a Fly Machine in app", os.Getenv("FLY_APP_NAME"))
+    time.Sleep(50 * time.Millisecond)
+    println(" I'm about to shut down!")
+    time.Sleep(50 * time.Millisecond)
+    println(" Run this Machine again:")
+    time.Sleep(50 * time.Millisecond)
+    println(" ---")
+    time.Sleep(50 * time.Millisecond)
+    println(" fly m start", os.Getenv("FLY_ALLOC_ID"))
+    time.Sleep(50 * time.Millisecond)
+    println(" ---")
+    time.Sleep(50 * time.Millisecond)
+    println(" Goodbye")
+    time.Sleep(50 * time.Millisecond)
+    println(" ===============================")
 }
 
 func main() {
-    defer time.Sleep(100 * time.Millisecond)
+    // defer time.Sleep(100 * time.Millisecond)
     printlogs()
+    
 }
